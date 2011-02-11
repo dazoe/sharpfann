62298bccd471f7e29b350b976d3751b99f2f92eb
jamuup@gmail.com <jamuup@gmail.com@c7d14904-1cf9-7f93-4281-05a170a2762c>
2011-02-11 23:16:30 +0000

    First version of port. Not exactly tested well.  Just one run.

---
+++ TestHarness/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpFANN;
+
+namespace TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Layer> layers = new List<Layer>();
+            layers.Add(Layer.Create(2));
+            layers.Add(Layer.Create(3, ActivationFunction.FANN_SIGMOID_SYMMETRIC));
+            layers.Add(Layer.Create(1, ActivationFunction.FANN_SIGMOID_SYMMETRIC));
+
+            using (FannStandard fann = new FannStandard(layers))
+            {
+                Trainer trainer = new Trainer(fann);
+
+                float desiredError = 0.001f;
+                float mse = trainer.Train(@"TestFiles\xor.data", 500000, 100, desiredError);
+                Console.WriteLine(mse.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
